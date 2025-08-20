14a29eab5016a571ee4f763b71d9f29f12051710
kwak <kmj5587482@naver.com>
2025-08-20 21:05:47 +0900

    add [main]

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,25 +1,25 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-    const [message, setMessage] = useState("");
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("");
 
-useEffect(() => {
+  useEffect(() => {
     fetch("http://localhost:8080/api/data/hello")
-      .then(response => {
+      .then((response) => {
         if (!response.ok) {
           throw new Error("서버 응답 에러");
         }
         return response.json(); // JSON으로 변환
       })
-      .then(data => {
+      .then((data) => {
         console.log("서버 응답:", data);
         setMessage(data.message); // 스프링에서 내려준 message 사용
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("에러 발생:", error);
       });
   }, []); // 컴포넌트 처음 렌더링될 때 실행
@@ -32,4 +32,4 @@ useEffect(() => {
   );
 }
 
-export default App
+export default App;

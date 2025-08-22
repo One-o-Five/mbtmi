4115e9b0e0871a5784d3d186e324f5dc6f2450da
kwak <kmj5587482@naver.com>
2025-08-22 12:46:19 +0900

    add [like give&take]

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,16 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Home from "./main/home";
-import TodayPost from "./today's_post/TodayPost";
-import Account from "./account/account01";
-
-import TodayPost from "./today's_post/TodayPost.jsx";
+import Activity from "./activities/ActivityGive";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Home />
-    <TodayPost/>
-    <Account/>
-  </StrictMode>,
-)
+    <Activity />
+  </StrictMode>
+);

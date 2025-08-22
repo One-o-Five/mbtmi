4af5d8ff88c1d6561af2a191cbff8660d31bf21e
ldj412 <2zinomikai@naver.com>
2025-08-22 12:05:13 +0900

    [add]  AccountInfo, AccountSelMbti, AccountYear

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,16 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Home from "./main/home";
-import TodayPost from "./today's_post/TodayPost";
-import Account from "./account/account01";
 
-import TodayPost from "./today's_post/TodayPost.jsx";
+import AccountInfo from "./account/AccountInfo";
+import AccountSelMbti from "./account/AccountSelMbti";
+import Account from "./account/Account01";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Home />
-    <TodayPost/>
-    <Account/>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);

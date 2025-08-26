0c661fba91ed3ed9da1709b594a31682d6c6c9a0
kwak <kmj5587482@naver.com>
2025-08-26 09:55:52 +0900

    [add] chatting page, myinfo page, activity page

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Activity from "./activities/ActivityGive";
+import ChattingProfile from "./chatting/ChattingProfile";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Activity />
+    <ChattingProfile />
   </StrictMode>
 );

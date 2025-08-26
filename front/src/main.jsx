44960bce4dac3747db9bc7c6cedcdb271ea2b7ce
youngin <yyiyyh1@naver.com>
2025-08-26 09:58:57 +0900

    no message

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,14 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import TodayPost from "./today's_post/TodayPost";
-import TodayTmi from "./today's_tmi/TodayTmi";
-import Map from "./map/Map";
 
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        {/* <TodayPost /> */}
-        {/* <TodayTmi /> */}
-        <Map />
-    </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);

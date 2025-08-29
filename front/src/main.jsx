75655345144dd7d0ea81c9fdfb9bbec5952c63b7
kimjaehyeong <comslzhslzh@naver.com>
2025-08-29 17:54:49 +0900

    [modify] Account01, info, main

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AccountMain from "./account/AccountMain";
 import { AuthProvider } from "./main/AuthContext";
+import { GoogleOAuthProvider } from "@react-oauth/google"; // 추가
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:8080";
@@ -13,7 +14,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <AccountMain />
+        <GoogleOAuthProvider clientId="YOUR_CLIENT_ID_HERE"> {/* 여기에 발급받은 클라이언트ID */}
+          <AccountMain />
+        </GoogleOAuthProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
